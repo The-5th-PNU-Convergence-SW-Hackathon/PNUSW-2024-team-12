2d4c076efb626a1d8196eb3a5f8367e9018a30b1
minecoby <minecoby@naver.com>
2024-07-28 22:13:05 +0900

    토큰을통한 유저id호출

--- backend/history/history_router.py
+++ backend/history/history_router.py
@@ -1,6 +1,6 @@
 
 
-from fastapi import APIRouter, HTTPException, Depends
+from fastapi import APIRouter, HTTPException, Depends,Security
 from typing import List
 from database import get_historydb
 
@@ -8,14 +8,36 @@ from sqlalchemy.orm import Session
 from database import get_historydb
 from models import History as History
 from history_schema import HistoryCreate, HistoryResponse
-
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+import jwt
+import os
+from dotenv import load_dotenv
+load_dotenv()
+SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
+security = HTTPBearer()
 router = APIRouter(
     prefix="/history",
 )
+def decode_jwt(token: str):
+    print("decode_jwt")
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        print(f"Decoded payload: {payload}")  
+        return payload
+    except jwt.ExpiredSignatureError:
+        print("Token has expired")
+        raise HTTPException(status_code=401, detail="토큰이 만료되었습니다.")
+    except jwt.PyJWTError:
+        print("Invalid token")  
+        raise HTTPException(status_code=401, detail="인식되지 않는 토큰입니다.")
+    
 
 @router.post("/", response_model=HistoryResponse, tags=["history"])
-def create_post(history: HistoryCreate, db: Session = Depends(get_historydb)):
-    user_id = "11" #로직구현해야함
+def create_post(history: HistoryCreate,credentials: HTTPAuthorizationCredentials = Security(security), db: Session = Depends(get_historydb)):
+    token = credentials.credentials
+    payload = decode_jwt(token)
+    user_id = payload.get("sub")
     db_history = History(user_id= user_id, boarding_time=history.boarding_time, quit_time = history.quit_time,
                       amount=history.amount,depart=history.depart,dest=history.dest)
     db.add(db_history)
