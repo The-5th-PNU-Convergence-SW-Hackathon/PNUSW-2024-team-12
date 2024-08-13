e35a81e0bb524f47694e7c08ce31f65926fe0a92
minecoby <minecoby@naver.com>
2024-08-13 18:49:03 +0900

    main함수 수정

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
 from user.user_router import router as user_router # user router 불러오기
 from history.history_router import router as history_router # history router 불러오기
-
-import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
+from database import user_engine,history_engine,user_Base,history_Base
 app = FastAPI()
 
 
@@ -10,11 +10,17 @@ app = FastAPI()
 async def init():
     return {"init"}
 
+origins = ["*"]
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True)
-
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+user_Base.metadata.create_all(bind=user_engine)
+history_Base.metadata.create_all(bind=history_engine)
 # router 불러오기
 app.include_router(user_router, tags=["user"])
 app.include_router(history_router)
