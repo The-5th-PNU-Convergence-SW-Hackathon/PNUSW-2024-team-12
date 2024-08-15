203adf6d5191b2c3dc9a0d65dfb1357b04e1ff54
minecoby <minecoby@naver.com>
2024-08-15 21:48:33 +0900

    매칭시스템구축 테스트

--- backend/matching/matching_schema.py
+++ backend/matching/matching_schema.py
@@ -1,7 +1,40 @@
 from pydantic import BaseModel
+from typing import List
+from datetime import datetime
 
 class MatchingCreate(BaseModel):
-    boarding_time: str
+    matching_type: int
+    boarding_time: datetime
     depart: str
     dest: str
-    max_mem: int
+    max_member: int
+
+class MatchingResponse(BaseModel):
+    id: int
+    matching_type: int
+    boarding_time: datetime
+    depart: str
+    dest: str
+    max_member: int
+    current_member: int
+
+    class Config:
+        orm_mode = True
+
+class LobbyResponse(BaseModel):
+    id: int
+    depart: str
+    dest: str
+    max_member: int
+    current_member: int
+    created_by: int  # 방을 만든 사람의 ID
+
+    class Config:
+        orm_mode = True
+
+
+class LobbyListResponse(BaseModel):
+    lobbies: List[LobbyResponse]
+
+    class Config:
+        orm_mode = True
