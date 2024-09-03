b2bc3909f4d48576da6f0b1f414d13a770b01c48
hyojeong130 <hyojeong0109@pusan.ac.kr>
2024-09-04 01:03:01 +0900

    운행완료_유저

--- brr/lib/router/main_router.dart
+++ brr/lib/router/main_router.dart
@@ -29,7 +29,8 @@ import 'package:brr/view/driver_page/driver_accept_page_view.dart';
 import 'package:brr/view/driver_page/driver_mypage_view.dart';
 import 'package:brr/view/driver_page/driver_callstart_page_view.dart';
 import 'package:brr/view/driver_page/driver_decide_navigation_page_view.dart';
-import 'package:brr/view/driver_page/driver_complete_view.dart';  
+import 'package:brr/view/driver_page/driver_complete_view.dart';
+import 'package:brr/view/ride_complete_view.dart/ride_complete_view.dart';
 
 class MainRouter {
   static final List<GetPage> routes = [
@@ -165,5 +166,11 @@ class MainRouter {
                 matchingId: Get.arguments,
               ),
             )),
+            GetPage(
+      name: '/ridecomplete',
+      page: () => const MainLayout(
+        child: RideCompletePageView(),
+      ),
+    ),
   ];
 }
