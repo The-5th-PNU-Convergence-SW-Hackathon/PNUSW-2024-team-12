be9661b893883d7f82c5a8f66681da016a13b2cc
leejihe0n <leejiheon0516@pusan.ac.kr>
2024-09-04 14:13:33 +0900

    Update main.dart

--- brr/lib/main.dart
+++ brr/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter_naver_map/flutter_naver_map.dart';
 import 'dart:developer';
+
 void main() async{
   await dotenv.load(fileName: 'assets/config/.env');
   WidgetsFlutterBinding.ensureInitialized();
